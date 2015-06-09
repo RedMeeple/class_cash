3ff0cb07c194822ad331515f3d40c3131b49b04a
Anna Bedell <awbedell@gmail.com>
2015-06-09 17:04:46 -0400

    Instructor Login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def login
+    if request.post?
+      user = Instructor.find_by_email(params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to root_path, notice: "Login Successful"
+      else
+        flash.now[:notice] = "User and Password do not match our records."
+      end
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to sessions_login_path, notice: "Logout Successful"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/login'
+  post 'sessions/login'
+  get 'sessions/logout'
+
+  root 'students#new'
+
   resources :periods
   resources :students
   resources :jobs
