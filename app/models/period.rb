7bcc2137a43eb68b44e710bc7caa3adeb07b817f
Anna Bedell <awbedell@gmail.com>
2015-08-13 14:45:10 -0400

    new period works

--- app/models/period.rb
+++ app/models/period.rb
@@ -3,7 +3,7 @@ class Period < ActiveRecord::Base
   has_many :behaviors, through: :student
   has_many :bonuses, dependent: :destroy
   belongs_to :instructor
-  accepts_nested_attributes_for :students, reject_if: :all_blank
+  accepts_nested_attributes_for :students, reject_if: proc { |attributes| attributes['email'].blank? }
 
   def find_richest
     rich = self.students[0]
