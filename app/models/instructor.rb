5dbad2a20a36c97b8275ab4369e17c01e8b426dd
Anna Bedell <awbedell@gmail.com>
2015-08-13 13:43:56 -0400

    transaction index works

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -3,6 +3,7 @@ class Instructor < User
   has_many :students, through: :periods
   has_many :bonuses, through: :periods
   has_many :loans, through: :students
+  has_many :transactions, through: :students
 
   def find_students_with_award(award_type)
     self.students.select { |s| s.awards.map(&:award_type_id).include? award_type }
