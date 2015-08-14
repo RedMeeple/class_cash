a6b6175afe9bab68f71e8812b2d6200638369ffc
Anna Bedell <awbedell@gmail.com>
2015-08-14 19:12:53 -0400

    assign right ajax

--- app/controllers/rights_controller.rb
+++ app/controllers/rights_controller.rb
@@ -1,20 +1,16 @@
 class RightsController < ApplicationController
-  before_action :set_right, only: [:show, :edit, :update, :destroy]
+  before_action :set_right, only: [:show, :destroy]
   before_action :instructor_logged_in?
 
   def index
     @instructor = Instructor.find(current_user.id)
     @rights = @instructor.all_rights
     @new_rights = @instructor.unassigned_rights
+    @right = Right.new
   end
 
   def show
     @students = @right.students
-  end
-
-  def new
-    @instructor = Instructor.find(current_user.id)
-    @right = Right.new
   end
 
   def create
@@ -22,22 +18,9 @@ class RightsController < ApplicationController
 
     respond_to do |format|
       if @right.save
-        format.html { redirect_to @right, notice: 'Right was successfully created.' }
-        format.json { render :show, status: :created, location: @right }
+        format.js
       else
         format.html { render :new }
-        format.json { render json: @right.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @right.update(right_params)
-        format.html { redirect_to @right, notice: 'Right was successfully updated.' }
-        format.json { render :show, status: :ok, location: @right }
-      else
-        format.html { render :edit }
         format.json { render json: @right.errors, status: :unprocessable_entity }
       end
     end
@@ -46,6 +29,7 @@ class RightsController < ApplicationController
   def assign_right
     @student_right_assignment = StudentRightAssignment.find_by_student_id(params[:student_right_assignment][:student_id])
     @student_right_assignment.update(student_right_assignment_params)
+    @right = @student_right_assignment.right
     respond_to do |format|
       format.js
     end
@@ -63,14 +47,11 @@ class RightsController < ApplicationController
     params.require(:student_right_assignment).permit(:cash_level, :student_id, :right_id)
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_right
-      @right = Right.find(params[:id])
-    end
+  private def set_right
+    @right = Right.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def right_params
-      params.require(:right).permit(:description, :teacher_id)
-    end
+  private def right_params
+    params.require(:right).permit(:description, :teacher_id)
+  end
 end
