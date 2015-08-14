19d99869e7daefae192980fa9fa0f4aa98224d34
Anna Bedell <awbedell@gmail.com>
2015-08-14 13:35:17 -0400

    new right list

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -5,6 +5,7 @@ class Instructor < User
   has_many :loans, through: :students
   has_many :transactions, through: :students
   has_many :rights, dependent: :destroy
+  has_many :student_right_assignments, through: :students
 
   def find_students_with_award(award_type)
     self.students.select { |s| s.awards.map(&:award_type_id).include? award_type }
@@ -12,6 +13,10 @@ class Instructor < User
 
   def all_rights
     Right.where(instructor_id: [self.id, nil])
+  end
+
+  def unassigned_rights
+    self.student_right_assignments.where(right_id: nil)
   end
 
 end
