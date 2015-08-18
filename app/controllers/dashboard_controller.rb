9c0680772d0b083bf088babf51ca18a27b9322e3
Anna Bedell <awbedell@gmail.com>
2015-08-18 12:34:59 -0400

    added deatils to loans index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -24,8 +24,8 @@ class DashboardController < ApplicationController
     @transactions = @instructor.transactions.where("DATE(transactions.created_at) >= ?", Date.today).count
     @loans = @instructor.loans.where("DATE(loans.created_at) >= ?", Date.today).count
     @unentered_periods = @instructor.unentered_periods
-    @bonuses = Extra.where("DATE(extras.created_at) >= ?", Date.today).count
-    @class_bonuses = Bonus.where("DATE(bonuses.created_at) >= ?", Date.today).count
+    @bonuses = Extra.where("DATE(extras.created_at) >= ?", Date.today)
+    @class_bonuses = Bonus.where("DATE(bonuses.created_at) >= ?", Date.today)
   end
 
   private def nav_links_instructor
