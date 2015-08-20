5caf83c35972327f6d73179fff5552578999f08a
lee allen <leeallen337@gmail.com>
2015-08-20 19:28:05 -0400

    removed console logs

--- app/assets/javascripts/z_rights.js
+++ app/assets/javascripts/z_rights.js
@@ -17,10 +17,6 @@ app.rights = {
         var assignmentId = el.id.split('-')[1];
         var rightId = $(el).closest('div[id*="therightid"]').attr('id').split('-')[1];
         
-        console.log(assignmentId);
-        console.log(rightId);
-        console.log('/rights/assign/' + assignmentId + '/' + rightId);
-        
         $.ajax({
           url: '/rights/assign/' + assignmentId + '/' + rightId,
           type: 'PATCH',
