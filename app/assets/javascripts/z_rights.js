52ee90f029b62b686127726a493fc7bed7bf274f
Anna Bedell <awbedell@gmail.com>
2015-08-21 09:55:25 -0400

    merge

--- app/assets/javascripts/z_rights.js
+++ app/assets/javascripts/z_rights.js
@@ -2,34 +2,38 @@
 // # All this logic will automatically be available in application.js.
 
 app.rights = {
-  
+
   assignRights: function() {
     var dropZones = dragula([document.querySelector('#new-rights-list')]);
     var rights = document.querySelectorAll('div[id*="right-list"]');
-    
+
     for (var i = 0; i < rights.length; i++) {
       dropZones.containers.push(rights[i]);
     }
-    
+
     dropZones.on('drop', function(el) {
-      
+
       if (!$(el).closest('div[id="new-rights-list"]').length) {
         var assignmentId = el.id.split('-')[1];
         var rightId = $(el).closest('div[id*="therightid"]').attr('id').split('-')[1];
-        
+
         console.log(assignmentId);
         console.log(rightId);
         console.log('/rights/assign/' + assignmentId + '/' + rightId);
-        
+
         $.ajax({
           url: '/rights/assign/' + assignmentId + '/' + rightId,
           type: 'PATCH',
           success: function(data) {
             $('#right-row' + rightId).html(data);
+          },
+          error: function(request, error) {
+            console.log(error)
+            console.log(request)
           }
         })
       }
-      
+
     })
   }
 }
