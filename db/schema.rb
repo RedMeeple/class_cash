6489043ec18e6ccebbb502308ce6cd4036b94a89
Anna Bedell <awbedell@gmail.com>
2015-08-23 20:36:39 -0400

    teachers can disable student accounts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818153108) do
+ActiveRecord::Schema.define(version: 20150824000807) do
 
   create_table "award_types", force: :cascade do |t|
     t.string   "name"
@@ -146,6 +146,7 @@ ActiveRecord::Schema.define(version: 20150818153108) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "disabled"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
