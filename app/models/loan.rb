66f9557250d4e7e289bccbde89de42e68311be8e
Anna Bedell <awbedell@gmail.com>
2015-10-13 07:26:47 -0400

    automatic loan payment bug fix

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -26,8 +26,12 @@ class Loan < ActiveRecord::Base
 
   def calculate_new_balance
     if (Date.today != self.start_date) && ((Date.today - self.start_date) % 7 == 0)
-      self.update(balance: (self.balance + (self.balance * self.interest.to_f / 100)))
-      make_payment
+      if self.accepted == true 
+        self.update(balance: (self.balance + (self.balance * self.interest.to_f / 100)))
+        make_payment
+      else
+        Loan.find(self.id).destroy
+      end
     end
   end
 
