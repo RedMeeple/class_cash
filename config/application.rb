36e3974f0be5464d65805a98d7208d0a470ea78c
Anna Bedell <awbedell@gmail.com>
2015-10-20 08:02:03 -0400

    require rack cors

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rack/cors'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -28,7 +29,7 @@ module ClassCash
     config.middleware.insert_before 0, "Rack::Cors" do
        allow do
          origins '*'
-         resource '*', headers: :any, methods: [:get, :post, :patch, :options]
+         resource '*', headers: :any, methods: [:get, :post, :patch, :options, :put]
        end
     end
 
